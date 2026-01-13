fff0d1d68d5796518dd210d3ebc7c2741f590a23
Kritika Jain <jainkritika2020@gmail.com>
2026-01-13 18:30:10 +0530

    Modified backend

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -100,7 +100,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
