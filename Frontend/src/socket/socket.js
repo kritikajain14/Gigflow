d1c217a73ab49f3c852f3258db8d405ea1a9990f
Kritika Jain <jainkritika2020@gmail.com>
2026-01-13 22:50:57 +0530

    modified withCredentials

--- Frontend/src/socket/socket.js
+++ Frontend/src/socket/socket.js
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client';
 const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 export const socket = io(SOCKET_URL, {
-  withCredentials: true,
-  autoConnect: false 
+  withCredentials: false,
+  transports: ['websocket'] 
 });
